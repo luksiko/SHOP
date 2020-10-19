84ad65e8417e4e6b9d56ca179e8a90386ee7dfd7
Aleksey <luksiko90@gmail.com>
2020-10-19 17:01:36 +0500

    23. Handling User Input (Добавили useState для редактирования TextInput. также проверку на то редактируется эта страница или создается )

--- src/screens/user/EditProductScreen.js
+++ src/screens/user/EditProductScreen.js
@@ -1,14 +1,106 @@
-import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, Text, TextInput, Platform } from 'react-native'
+import { useSelector } from 'react-redux'
+import { ScrollView } from 'react-native-gesture-handler'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+
+import CustomHeaderButton from '../../components/UI/HeaderButton'
 
 const EditProductScreen = (props) => {
+	const prodId = props.navigation.getParam('productId')
+	const editedProduct = useSelector((state) =>
+		state.produsts.userProducts.find((prod) => prod.id === prodId),
+	)
+
+	const [title, setTitle] = useState(editedProduct ? editedProduct.title : '')
+	const [imageUrl, setImageUrl] = useState(
+		editedProduct ? editedProduct.imageUrl : '',
+	)
+	const [price, setPrice] = useState(editedProduct ? editedProduct.price : '')
+	const [description, setDescription] = useState(
+		editedProduct ? editedProduct.description : '',
+	)
+
 	return (
-		<View style={styles.editProd}>
-			<Text>EditProductScreen</Text>
-		</View>
+		<ScrollView>
+			<View style={styles.form}>
+				<View style={styles.formControl}>
+					<Text style={styles.label}>Title</Text>
+					<TextInput
+						style={styles.input}
+						value={title}
+						onChangeText={(text) => setTitle(text)}
+					/>
+				</View>
+				<View style={styles.formControl}>
+					<Text style={styles.label}>Image URL</Text>
+					<TextInput
+						style={styles.input}
+						value={imageUrl}
+						onChangeText={(text) => setImageUrl(text)}
+					/>
+				</View>
+				{editedProduct ? null : (
+					<View style={styles.formControl}>
+						<Text style={styles.label}>Price</Text>
+						<TextInput
+							style={styles.input}
+							value={price}
+							onChangeText={(text) => setPrice(text)}
+						/>
+					</View>
+				)}
+				<View style={styles.formControl}>
+					<Text style={styles.label}>Description</Text>
+					<TextInput
+						style={styles.input}
+						value={description}
+						onChangeText={(text) => setDescription(text)}
+					/>
+				</View>
+			</View>
+		</ScrollView>
 	)
 }
 
-const styles = StyleSheet.create({ editProd: {} })
+EditProductScreen.navigationOptions = (navData) => {
+	const submitFn = navData.navigation.getParam('submit')
+	return {
+		// если передали id товара для редактирования, то выдаем Edit, иначе Add
+		headerTitle: navData.navigation.getParam('productId')
+			? 'Edit Product'
+			: 'Add Product',
+		headerRight: (
+			<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+				<Item
+					title='Save'
+					iconName={
+						Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'
+					}
+					onPress={submitFn}
+				/>
+			</HeaderButtons>
+		),
+	}
+}
+
+const styles = StyleSheet.create({
+	form: {
+		margin: 20,
+	},
+	formControl: {
+		width: '100%',
+	},
+	label: {
+		fontFamily: 'open-sans-bold',
+		marginVertical: 8,
+	},
+	input: {
+		paddingHorizontal: 2,
+		paddingVertical: 5,
+		borderBottomColor: '#ccc',
+		borderBottomWidth: 1,
+	},
+})
 
 export default EditProductScreen
