dbffb713107d66c5cff1599d90a4cabf79bc7a52
Aleksey <luksiko90@gmail.com>
2020-10-26 19:37:46 +0500

    8. Updating & Deleting Products

--- src/store/actions/products.js
+++ src/store/actions/products.js
@@ -38,7 +38,10 @@ export const fetchProducts = () => {
 }
 
 export const deleteProduct = productId => {
-	return { type: DELETE_PRODUCT, pid: productId }
+	return async dispatch => {
+		await fetch(`https://rn-guide-shop.firebaseio.com/products/${productId}.json`, { method: 'DELETE' })
+		dispatch({ type: DELETE_PRODUCT, pid: productId })
+	}
 }
 
 export const createProduct = (title, description, imageUrl, price) => {
@@ -67,13 +70,23 @@ export const createProduct = (title, description, imageUrl, price) => {
 }
 
 export const updateProduct = (id, title, description, imageUrl) => {
-	return {
-		type: UPDATE_PRODUCT,
-		pid: id,
-		productData: {
-			title,
-			description,
-			imageUrl,
-		},
+	return async dispatch => {
+		await fetch(`https://rn-guide-shop.firebaseio.com/products/${id}.json`, {
+			method: 'PATCH', // or 'PUT'(обновит весь обьект)
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ title, description, imageUrl }),
+		})
+
+		dispatch({
+			type: UPDATE_PRODUCT,
+			pid: id,
+			productData: {
+				title,
+				description,
+				imageUrl,
+			},
+		})
 	}
 }
